72b33bdb769e12eeb09ff159ed7029ab59c744c7
johnvictorfs <johnvictorfs@gmail.com>
2018-09-09 22:45:32 -0300

    Added command to set raids day from discord.

---
+++ bot/cogs/raids_day.py
@@ -0,0 +1,33 @@
+# Non-Standard lib imports
+from discord.ext import commands
+
+
+def check_role(ctx, *roles):
+    for role in roles:
+        if role in str(ctx.message.author.roles):
+            return True
+    return False
+
+
+class RaidsDay:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def set_raids_day(self, ctx, *, day):
+        print(f"{ctx.author} has issued set_raids_day command.")
+        if not check_role(ctx, "Mod+", "Admin"):
+            print("D E N I E D")
+            return
+        else:
+            pass
+
+        day = int(day)
+        with open("dia_de_raids.txt", "w") as f:
+            f.write(f"; 0 = par, 1 = ímpar\n"
+                    f"{day}")
+
+
+def setup(bot):
+    bot.add_cog(RaidsDay(bot))
