471924bc10a9c9c711d3a0d4fbf88f3fcb0a4933
johnvictorfs <johnvictorfs@gmail.com>
2019-04-04 00:50:15 -0300

    Fixed alembic migrations not getting db url from env variables.

--- alembic/env.py
+++ alembic/env.py
@@ -19,6 +19,8 @@ from bot.orm import models
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
