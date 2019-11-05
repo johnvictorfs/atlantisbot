c98f25a239b1be998ff440e8485074aeab0c5410
John Victor Sampaio <johnvictorfs@gmail.com>
2019-11-05 13:16:18 -0300

    Increase cooldown after error when checking users

--- bot/cogs/authentication.py
+++ bot/cogs/authentication.py
@@ -149,7 +149,7 @@ class UserAuthentication(commands.Cog):
                         if not user_data:
                             self.error_logger.error(f'[check_users] sem user_data para {user}.')
                             # Sometimes call to RS3's API fail and a 404 html page is returned instead (...?)
-                            await asyncio.sleep(60)
+                            await asyncio.sleep(600)
                             continue
 
                         if not self.debugging and user_data.get('clan') == self.bot.setting.clan_name:
